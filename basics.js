6283836572fe76d320a51949d9f895b28b9656ad
Theo Tran <theotran@Theos-MacBook-Pro.local>
2015-10-28 16:00:50 -1000

    Finished doing all the JS exercises

--- basics.js
+++ basics.js
@@ -43,3 +43,27 @@ canDrive(person.age);
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+
+var dog = {
+ 	name: 'Spot',
+ 	type: "German Shepherd",
+ 	color: "Brown",
+ 	age: 2,
+
+};
+
+
+function bark (call) {
+	if (dog.name === call) {
+		console.log("ruff ruff!!!");
+	}
+}
+
+console.log(bark(dog.name));
+
+
+
+
+
+
